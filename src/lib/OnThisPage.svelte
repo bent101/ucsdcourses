47c314b5dd1f40b50dd90ca2c6cc520643f360c7
Ben Tomlin <bentomlin101@gmail.com>
2023-03-06 22:35:30 -0800

    fixed sticky headers

--- src/lib/OnThisPage.svelte
+++ src/lib/OnThisPage.svelte
@@ -2,29 +2,63 @@
 	import { afterNavigate } from "$app/navigation";
 
 	const styles = new Map([
-		["H2", "border-gray-700 hover:text-gray-200 border bg-gray-800 py-1.5 top-0 sticky my-1 text-gray-500 font-extrabold tracking-wide"],
-		["H3", "border-gray-700 hover:text-gray-200 border bg-gray-800 py-1.5 top-0 sticky my-1 text-gray-500 font-semibold"],
-		["H4", "border-gray-700 hover:text-gray-200 border bg-gray-800 py-1.5 top-0 sticky my-1 text-gray-500"],
-		["H5", "text-gray-400 hover:text-gray-200"]
+		["H2", "border border-gray-700 bg-[#283140] text-gray-400 font-extrabold tracking-wide"],
+		["H3", "border border-gray-700 bg-gray-800 text-gray-500 font-extrabold tracking-wide"],
+		["H4", "border border-gray-700 bg-gray-800 text-gray-500"],
+		["H5", "text-gray-400"]
 	]);
 
-	let links = [] as { href: string; text: string; styles: string }[];
+	type Link = {
+		href: string;
+		text: string;
+		styles: string;
+	};
+
+	type Group = {
+		header: Link | undefined;
+		content: Link[];
+		level: number;
+	};
+
+	let groups: Group[] = [];
+	let a = 0;
+	let ul: Element;
+
 	afterNavigate(() => {
-		links = Array.from(document.querySelectorAll("h2,h3,h4,h5")).map((node) => ({
-			href: node.id ? "#" + node.id : "",
-			text: node.textContent ?? "",
-			styles: styles.get(node.tagName) ?? ""
-		}));
-		console.log(links);
+		ul.scrollTop = 0;
+		a = 1;
+		groups = [{ header: undefined, content: [], level: 0 }];
+		for (const node of document.querySelectorAll("h2,h3,h4,h5")) {
+			const link = {
+				href: node.id ? "#" + node.id : "",
+				text: node.textContent ?? "",
+				styles: styles.get(node.tagName) ?? ""
+			};
+			if (node.tagName === "H5") {
+				groups[groups.length - 1].content.push(link);
+			} else {
+				groups.push({ header: link, content: [], level: +node.tagName[1] - 2 });
+			}
+		}
+
+		console.log(groups);
 	});
 </script>
 
-<span class="absolute left-4 top-[7.5rem] rounded-full bg-gray-800 px-2 text-sm font-bold uppercase tracking-wider text-gray-500">On this page</span>
-<ul class="h-full overflow-y-scroll rounded-lg border border-gray-700 p-4 pb-0">
-	<li class="mt-2">
-		{#each links as { href, text, styles }, i}
-			<a {href} class="{styles} block rounded-md px-2 py-1 hover:bg-gray-700">{text}</a>
-		{/each}
-	</li>
+<ul bind:this={ul} class="relative h-full overflow-y-scroll rounded-lg border border-gray-700 p-4 pb-0">
+	{#each groups as { header, content, level }}
+		<li class:ml-2={level === 1} class:ml-4={level === 2} class:ml-6={level === 3} class="mt-2">
+			{#if header !== undefined}
+				<a href={header.href} class="{header.styles} sticky top-0 my-1 block rounded-md px-2 py-1.5 shadow-lg hover:bg-gray-700 hover:text-white">{header.text}</a>
+			{/if}
+			<div>
+				{#each content as { href, text, styles }}
+					<a {href} class="{styles} block rounded-md px-2 py-1 hover:bg-gray-700 hover:text-white">{text}</a>
+				{/each}
+			</div>
+		</li>
+	{/each}
 	<div class="pointer-events-none sticky inset-x-0 bottom-0 h-24 bg-gradient-to-b from-transparent to-gray-800" />
 </ul>
+
+<span class="absolute left-4 top-[7.5rem] rounded-full bg-gray-800 px-2 text-sm font-bold uppercase tracking-wider text-gray-500">On this page</span>
